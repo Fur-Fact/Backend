f141493d28f3010119a27169b00121f4106a2b79
kimich <ickim1218@gmail.com>
2024-07-11 18:16:34 +0900

    feat: db 연동 세팅

---
+++ src/data/initDatabase.js
@@ -0,0 +1,25 @@
+const sequelize = require('./sequelize');
+const User = require('../api/user/model');
+const Pet = require('../api/pet/model');
+const Test = require('../api/test/model');
+const FurData = require('../api/furdata/model');
+
+// User와 Pet 간의 관계 설정
+User.hasMany(Pet, { foreignKey: 'user_id' });
+Pet.belongsTo(User, { foreignKey: 'user_id' });
+
+// Test와 FurData 간의 관계 설정
+Test.hasMany(FurData, { foreignKey: 'test_id' });
+FurData.belongsTo(Test, { foreignKey: 'test_id' });
+
+async function initializeDatabase() {
+    try {
+        await sequelize.sync({ force: true });
+        console.log('Database & tables created!');
+    } catch (err) {
+        console.error('Unable to connect to the database:', err);
+        throw err; // 에러 발생 시 예외를 던져 서버 시작을 중단하도록 함
+    }
+}
+
+module.exports = initializeDatabase;
