4dcf04f9a9a1488089c625afe74e76f0b29ac50c
kimich <ickim1218@gmail.com>
2024-07-22 18:26:14 +0900

    fix: 전화번호와 펫 이름으로 test 조회

--- src/api/pet/repository.js
+++ src/api/pet/repository.js
@@ -1,5 +1,6 @@
 const Pet = require("./model");
 const Test = require("../test/model");
+const Furdata = require("../furdata/model");
 
 exports.createPet = async (petData) => {
   return await Pet.create(petData);
@@ -21,20 +22,7 @@ exports.getPet = async (petId) => {
   return await Pet.findByPk(petId);
 };
 
-exports.getPetsByUserNameAndPetName = async (userName, petName) => {
-  try {
-    const pets = await Test.findAll({
-      where: {
-        guardian_name: userName,
-        pet_name: petName,
-      },
-    });
-    return pets;
-  } catch (err) {
-    console.error('Error fetching pets by user name and pet name:', err);
-    throw err;
-  }
-};
+
 
 exports.getPetsByUserId = async (userId) => {
   try {
--- src/router.js
+++ src/router.js
@@ -19,7 +19,6 @@ router.get("/api/v1/users/:id", userController.getUserById); //유저별 조회
 
 //펫
 router.post("/api/v1/pets", verify, petController.registerPet); // 펫 등록
-router.get('/api/v1/pets/search', petController.getPetByName);
 router.get('/api/v1/pets/list', verify, petController.getPetsByUserId);
 router.put("/api/v1/pets/:petId", verify, petController.updatePet); // 펫 정보 수정
 router.delete("/api/v1/pets/:petId", verify, petController.deletePet); // 펫 정보 삭제
@@ -31,9 +30,11 @@ router.post("/api/v1/tests", testController.createTest);
 router.post('/api/v1/tests/comment', testController.addCommentToTest);
 router.post('/api/v1/tests/push', testController.pushAlarm);
 router.post("/api/v1/tests", testController.createTest); // 검사 정보 추가
+router.get('/api/v1/tests',verify, testController.getTestList); // 검사 리스트 조회
+router.get('/api/v1/tests/search', verify, testController.getTestByPhoneAndName);
 router.get('/api/v1/tests/:testId',verify, testController.getTest); // 검사정보 조회
 router.delete('/api/v1/tests/:testId',verify, testController.deleteTest); // 검사정보 삭제
-router.get('/api/v1/tests',verify, testController.getTestList); // 검사 리스트 조회
+
 
 //furdata
 router.post(
